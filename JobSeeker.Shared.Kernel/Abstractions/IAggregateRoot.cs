a03cbe257873fb0a2aaa841c2c4bdb30b3207cba
behzad.b.i.g <behzad.b.i.g@gmail.com>
2025-10-30 07:48:18 +0330

    mt

---
+++ JobSeeker.Shared.Kernel/Abstractions/IAggregateRoot.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobSeeker.Shared.Kernel.Abstractions
+{
+    public interface IAggregateRoot { }
+}
---
+++ JobSeeker.Shared.Kernel/Domain/DomainEvent.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobSeeker.Shared.Kernel.Domain
+{
+    public abstract class DomainEvent
+    {
+        public DateTime OccurredOn { get; } = DateTime.UtcNow;
+    }
+}
