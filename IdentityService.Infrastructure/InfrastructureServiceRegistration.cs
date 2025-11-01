2405ee9103f051d48c5ca1a79d2f1fee96dd87c8
105624033 <behzad.b.i.g@gmail.com>
2025-11-01 13:22:07 +0330

    cmt

---
+++ IdentityService.Infrastructure/InfrastructureServiceRegistration.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IdentityService.Infrastructure.Jwt;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace IdentityService.Infrastructure
+{
+    public static class InfrastructureServiceRegistration
+    {
+        public static IServiceCollection ConfigureInfrastructureRegistrationServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddAuth();
+
+            return services;
+        }
+        private static IServiceCollection AddAuth(this IServiceCollection services)
+        {
+            services.AddSingleton<IJwtTokenGenerator, JwtService>();
+
+            return services;
+        }
+    }
+
+
+    public static class JWTAuthService{
+        public static IServiceCollection AddJWTService(this IServiceCollection services) {
+
+            services.AddAuthentication(opt =>
+            {
+                opt.DefaultAuthenticateScheme = JwtBrarerDefault
+            });
+            return services;
+        }
+    }
+}
