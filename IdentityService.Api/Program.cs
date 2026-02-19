d22aa717ed5c8dd4d1b8dcb2e2c2e87d9ef03d68
behzad.b.i.g <behzad.b.i.g@gmail.com>
2026-02-19 12:24:54 +0330

    cmt error removed

--- IdentityService.Api/Program.cs
+++ IdentityService.Api/Program.cs
@@ -28,7 +28,6 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Azure;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
-using ProfileService.Persistance.Messaging.Consumers;
 using Swashbuckle.AspNetCore.SwaggerGen;
 
 var builder = WebApplication.CreateBuilder(args);
